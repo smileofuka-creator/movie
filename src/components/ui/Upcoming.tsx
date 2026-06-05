30f8eb08c7a7f5a11f835837fd909fb2b4ceb776
smileofuka-creator <smileofuka@gmail.com>
2026-06-05 18:17:47 +0800

    chore: comment out unused components and code in Popular, TopRated, and Upcoming

--- src/components/ui/Upcoming.tsx
+++ src/components/ui/Upcoming.tsx
@@ -1,34 +1,34 @@
-import { MoveRight } from "lucide-react";
-import MovieCard from "./MovieCard";
-interface UpcomingProps {
-  movies: any[];
-}
-const Upcoming = ({ movies = [] }: UpcomingProps) => {
-  return (
-    <>
-      <div className="max-w-[1440px] items-center mx-auto px-4 md:px-16 py-10">
-        <div className="w-full flex justify-between items-center">
-          <h3 className=" text-2xl font-bold text-black mb-6">Upcoming</h3>
-          <div className="flex items-center gap-2 cursor-pointer text-sm font-medium hover:text-gray-600 transition-colors">
-            <span>See more</span>
-            <MoveRight className="w-[9.33px] h-[9.33px]"></MoveRight>
-          </div>
-        </div>
+// import { MoveRight } from "lucide-react";
+// import MovieCard from "./MovieCard";
+// interface UpcomingProps {
+//   movies: any[];
+// }
+// const Upcoming = ({ movies = [] }: UpcomingProps) => {
+//   return (
+//     <>
+//       <div className="max-w-[1440px] items-center mx-auto px-4 md:px-16 py-10">
+//         <div className="w-full flex justify-between items-center">
+//           <h3 className=" text-2xl font-bold text-black mb-6">Upcoming</h3>
+//           <div className="flex items-center gap-2 cursor-pointer text-sm font-medium hover:text-gray-600 transition-colors">
+//             <span>See more</span>
+//             <MoveRight className="w-[9.33px] h-[9.33px]"></MoveRight>
+//           </div>
+//         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 justify-items-center">
-          {movies.length > 0 &&
-            movies.map((movie) => (
-              <MovieCard
-                key={movie.id}
-                title={movie.name}
-                rating={movie.vote_average}
-                imageUrl={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-              />
-            ))}
-        </div>
-      </div>
-    </>
-  );
-};
+//         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 justify-items-center">
+//           {movies.length > 0 &&
+//             movies.map((movie) => (
+//               <MovieCard
+//                 key={movie.id}
+//                 title={movie.name}
+//                 rating={movie.vote_average}
+//                 imageUrl={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+//               />
+//             ))}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
 
-export default Upcoming;
+// export default Upcoming;
